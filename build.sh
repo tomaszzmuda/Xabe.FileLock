7eea3f298a02cde26bf771fa794ec0b7f26feda8
Tomasz Żmuda <mundek@test.xabe>
2017-06-30 05:25:48 -0500

    Build fix

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [[ -z "${TRAVIS_TAG}" ]]; then
 	exit 0
 else
 	dotnet pack Xabe.FileLock -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG
-	dotnet nuget push Xabe.FileLock/nuget/*.nupkg -k $NUGET_API https://www.nuegt.org/api/v2/package
+	dotnet nuget push Xabe.FileLock/nuget/*.nupkg -k $NUGET_API -s https://www.nuegt.org/api/v2/package
 fi
